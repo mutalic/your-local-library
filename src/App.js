67622e1be7d4b0b078d638f029c23a898b5018a7
Jason <jasonee96@gmail.com>
2023-06-07 20:02:40 -0500

    Filter-by-availability functionality added.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import "./App.css";
+import bookData from "./data/books";
+import React from "react";
+import { useState, useEffect } from "react";
 
 function App() {
+  // State
+  let [books, setBooks] = useState(bookData);
+  let [isChecked, setIsChecked] = useState(false);
+
+  function filterAvailableBooks(books) {
+    return books.filter(function (book) {
+      return book.availability;
+    });
+  }
+
+  useEffect(() => {
+    console.log(isChecked);
+    if (isChecked) {
+      setBooks(filterAvailableBooks(books));
+    }
+    if (!isChecked) {
+      setBooks(bookData);
+    }
+  }, [isChecked]);
+
   return (
     <main className="grid-container">
       {/* Nav Bar */}
+
       <div className="nav">
         <h1 className="nav__title">Your Local Library</h1>
         <ul className="nav__menu">
@@ -18,7 +42,7 @@ function App() {
 
       {/* Side Bar */}
       <div className="side-bar">
-        <div className="side-bar__search--container">
+        <div className="side-bar__search--container bold">
           <label htmlFor="search-input">Booksearch </label>
           <input
             id="search-input"
@@ -29,42 +53,53 @@ function App() {
         </div>
         <div className="side-bar__filter">
           <form className="side-bar__filter--container">
-            <h3>Filter</h3>
-            <div className="side-bar__filter--availability checkbox">
+            <h3 className="bold">Filter</h3>
+            <div
+              className="side-bar__filter--availability checkbox-container"
+              onClick={() => {
+                setIsChecked(!isChecked);
+              }}
+            >
               <label htmlFor="filter-availability">Availability</label>
-              <input type="checkbox" id="filter-availability" />
+              <input
+                type="checkbox"
+                id="filter-availability"
+                className="checkbox"
+                checked={isChecked}
+              />
             </div>
-            <div className="side-bar__filter--security-deposit checkbox">
+            {/* <div className="side-bar__filter--security-deposit checkbox-container">
               <label htmlFor="filter-security-deposit">Security Deposit</label>
-              <input type="checkbox" id="filter-security-deposit" />
-            </div>
+              <input
+                type="checkbox"
+                id="filter-security-deposit"
+                className="checkbox"
+              />
+            </div> */}
           </form>
         </div>
       </div>
+      {/* Books */}
       <div className="books">
-        <div className="book">
-          <div className="book__img"></div>
-          <div className="book__info">
-            <h4 className="book__title">Pride and Prejudice</h4>
-            <h5 className="book__author">Jane Austen</h5>
-            <h6 className="book__publisher">Penguin Classics</h6>
-          </div>
-        </div>
+        {books.map(function (book) {
+          return (
+            <div className="book" id={book.id}>
+              <div className="book__img">
+                <img src={book.image} alt="book cover" width="100%" />
+              </div>
+              <div className="book__info">
+                <h4 className="book__info--title bold">{book.title}</h4>
+                <h5 className="book__info--author">{book.author}</h5>
+                <h6 className="book__info--publisher">{book.publisher}</h6>
+                <p className="book__info--availability">
+                  {book.availability ? "Available" : "Unavailable"}
+                </p>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </main>
-  );
-}
-
-function Book() {
-  return (
-    <div className="book">
-      <div className="book__img"></div>
-      <div className="book__info">
-        <h4 className="book__title">Pride and Prejudice</h4>
-        <h5 className="book__author">Jane Austen</h5>
-        <h6 className="book__publisher">Penguin Classics</h6>
-      </div>
-    </div>
   );
 }
 
--- src/data/books.js
+++ src/data/books.js
@@ -1,8 +1,11 @@
 let books = [
   {
+    id: 0,
     title: "Animal Farm",
     author: "George Orwell",
-    image: "",
+    image:
+      "https://m.media-amazon.com/images/I/51-IWZeA-qL._SX312_BO1,204,203,200_.jpg",
+    publisher: "Penguin Classics",
     lender: {
       name: "Thomas Lunsford",
       location: "Fayetteville, AR",
@@ -11,9 +14,12 @@ let books = [
     deposit: 5,
   },
   {
+    id: 1,
     title: "The Great Gatsby",
     author: "F. Scott Fitzgerald",
-    image: "",
+    image:
+      "https://m.media-amazon.com/images/I/41NssxNlPlS._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+    publisher: "The Original 1925 Edition",
     lender: {
       name: "Jason Hwang",
       location: "Springdale, AR",
@@ -22,9 +28,12 @@ let books = [
     deposit: 7,
   },
   {
+    id: 2,
     title: "Great Expectations",
     author: "Charles Dickens",
-    image: "",
+    image:
+      "https://m.media-amazon.com/images/I/51i0v6yD7aL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+    publisher: "Penguin Clothbound Classics",
     lender: {
       name: "Emily Davis",
       location: "Tonitown, AR",
@@ -33,9 +42,12 @@ let books = [
     deposit: 4,
   },
   {
+    id: 3,
     title: "Frankenstein",
     author: "Mary Shelly",
-    image: "",
+    image:
+      "https://m.media-amazon.com/images/I/41U8yWrDwZL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+    publisher: "Penguin Clothbound Classics",
     lender: {
       name: "Zack White",
       location: "Fayetteville, AR",
@@ -44,9 +56,12 @@ let books = [
     deposit: 10,
   },
   {
+    id: 4,
     title: "Wuthering Heights",
     author: "Emily Bronte",
-    image: "",
+    image:
+      "https://m.media-amazon.com/images/I/51xUzDnJeAL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+    publisher: "Independantly Published",
     lender: {
       name: "Zoe Reif",
       location: "Rogers, AR",
@@ -55,9 +70,12 @@ let books = [
     deposit: 5,
   },
   {
+    id: 5,
     title: "Moby Dick",
     author: "Herman Melville",
-    image: "",
+    image:
+      "https://m.media-amazon.com/images/I/41NG9esW3CL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+    publisher: "Macmillan Collector's Library",
     lender: {
       name: "Patrick Graham",
       location: "Rogers, AR",
@@ -66,9 +84,12 @@ let books = [
     deposit: 20,
   },
   {
-    title: "The Lord of the Rings",
+    id: 6,
+    title: "The Hobbit",
     author: "J.R.R. Tolkien",
-    image: "",
+    image:
+      "https://m.media-amazon.com/images/I/413V3sIKSJL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+    publisher: "Houghton Mifflin Harcourt",
     lender: {
       name: "Aidan Brennan",
       location: "Fayetteville, AR",
@@ -77,20 +98,26 @@ let books = [
     deposit: 5,
   },
   {
+    id: 7,
     title: "The Grapes of Wrath",
     author: "John Steinbeck",
-    image: "",
+    image:
+      "https://m.media-amazon.com/images/I/51QyaWppYJL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+    publisher: "Viking",
     lender: {
       name: "Jason Hwang",
       location: "Springdale, AR",
     },
     availability: true,
-    deposit: 8,
+    deposit: 30,
   },
   {
+    id: 8,
     title: "Song of Solomon",
     author: "Toni Morrison",
-    image: "",
+    image:
+      "https://m.media-amazon.com/images/I/414zTKD+0UL._SY344_BO1,204,203,200_.jpg",
+    publisher: "Vintage",
     lender: {
       name: "Jason Hwang",
       location: "Springdale, AR",
@@ -99,9 +126,12 @@ let books = [
     deposit: 13,
   },
   {
+    id: 9,
     title: "Fahrenheit 451",
     author: "Ray Bradbury",
-    image: "",
+    image:
+      "https://m.media-amazon.com/images/I/41oWDDRb2cL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+    publisher: "Simon & Schuster",
     lender: {
       name: "Kath Reese",
       location: "Fayetteville, AR",
